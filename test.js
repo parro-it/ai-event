466bd52c1908dd60b6f5c772315bdd1912c7d7bc
Andrea Parodi <andrea@parro.it>
2017-11-28 17:13:44 +0100

    Add ai-concat dev dep

--- test.js
+++ test.js
@@ -1,17 +1,8 @@
 import test from "tape-async";
 import aiEvent from ".";
+import concat from "ai-concat";
 import { createReadStream } from "fs";
 const fromStream = stream => aiEvent(stream, "data");
-
-async function concat(iterable) {
-  let result = "";
-
-  for await (const chunk of iterable) {
-    result += chunk;
-  }
-
-  return result;
-}
 
 test("transform a stream into an async iterable", async t => {
   const stream = createReadStream(`${__dirname}/fixtures/test`, "utf8");
